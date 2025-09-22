cb8f0d430c4d251877326149c67149fa2dc88bf7
jordan <sudo.sendmail.jordan@gmail.com>
2025-09-22 14:38:30 -0700

    sync

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import seaborn
 target = 'stroke'
 numerical_labels = ['age', 'hypertension', 'heart_disease', 'avg_glucose_level', 'bmi']
 categorical_labels = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
-labels = ['genders','age','hypertension','heart_disease','ever_married','work_type','Residence_type','avg_glucose_level','bmi','smoking_status']
+labels = ['gender','age','hypertension','heart_disease','ever_married','work_type','Residence_type','avg_glucose_level','bmi','smoking_status']
 coded_labels = ['age', 'hypertension', 'heart_disease', 'avg_glucose_level', 'bmi', 'gender_Male', 'ever_married_Yes', 'work_type_Non-working', 'work_type_Private', 'work_type_Self-employed', 'Residence_type_Urban', 'smoking_status_never smoked', 'smoking_status_smokes']
 
 model = joblib.load('random_forest_model.pkl')
@@ -33,15 +33,15 @@ if streamlit.session_state['page'] == 'Home':
         streamlit.header('Enter patient Information')
 
         gender = streamlit.selectbox('Gender', options=['Male','Female'])
-        age = streamlit.number_input('Age', min_value=0, max_value=42, value=0)
-        hypertension = streamlit.selectbox('Hypertension', options=['Yes', 'No'])
-        heart_disease = streamlit.selectbox('Heart Disease', options=['Yes', 'No'])
+        age = streamlit.number_input('Age', min_value=0.0, max_value=100.0, value=67.0)
+        hypertension = streamlit.selectbox('Hypertension', options=['0', '1'])
+        heart_disease = streamlit.selectbox('Heart Disease', options=['1', '0'])
         ever_married = streamlit.selectbox('Ever Married', options=['Yes', 'No'])
         work = streamlit.text_input('Work', value='Private')
         residence = streamlit.selectbox('Residence', options=['Urban', 'Rural'])
-        avg_glucose_level = streamlit.number_input('Average Glucose Level', min_value=0, max_value=300, value=100)
+        avg_glucose_level = streamlit.number_input('Average Glucose Level', min_value=0.0, max_value=300.0, value=228.69)
         bmi = streamlit.number_input('Body Mass Index', min_value=0.0, max_value=70.0, value=25.0, format='%.1f')
-        smoking_status = streamlit.selectbox('Smoking Status', options=['never smoked', 'formerly smoked'])
+        smoking_status = streamlit.selectbox('Smoking Status', options=['formerly smoked', 'never smoked',  'smokes'])
 
         if streamlit.form_submit_button('Submit'):
             streamlit.session_state['user_input'] = numpy.array([[
@@ -62,8 +62,20 @@ if streamlit.session_state['page'] == 'Home':
 
 
 elif streamlit.session_state['page'] == 'Result':
+    pandas.set_option('display.width', 1000)
+    pandas.set_option('display.max_colwidth', None)
+
     input_data = pandas.DataFrame(streamlit.session_state['user_input'], columns=labels)
-    input_data = pandas.get_dummies(X, columns=categorical_labels, drop_first=True)
+    print(input_data)
+    print(X.head(1))
+    
+    test = pandas.get_dummies(X.head(1), columns=categorical_labels)
+    print(test)
+
+
+    input_data = pandas.get_dummies(input_data, columns=categorical_labels)
+    input_data = input_data.reindex(columns=coded_labels, fill_value=0)
+    print(input_data)
 
     streamlit.title('Stroke Prediction Results')
 
@@ -107,7 +119,7 @@ elif streamlit.session_state['page'] == 'Result':
     fig2, ax2 = matplotlib.pyplot.subplots()
     ax2.hist(data_frame_cleand[data_frame_cleand['stroke'] == 0]['avg_glucose_level'], bins=20, alpha=0.5, label='no stroke')
     ax2.hist(data_frame_cleand[data_frame_cleand['stroke'] == 1]['avg_glucose_level'], bins=20, alpha=0.5, label='stroke')
-    ax2.axvline(input_data['avg_glucose_level'][0], color='red', linestyle='dashed', linewidth=2, label='patient avg_glucose_level')
+    ax2.axvline(int(float(input_data['avg_glucose_level'][0])), color='red', linestyle='dashed', linewidth=2, label='patient avg_glucose_level')
     ax2.set_xlabel('avg_glucose_level Level')
     ax2.set_ylabel('number of patients')
     ax2.legend()
@@ -118,7 +130,7 @@ elif streamlit.session_state['page'] == 'Result':
     fig3, ax3 = matplotlib.pyplot.subplots()
     ax3.hist(data_frame_cleand[data_frame_cleand['stroke'] == 0]['bmi'], bins=20, alpha=0.5, label='no stroke')
     ax3.hist(data_frame_cleand[data_frame_cleand['stroke'] == 1]['bmi'], bins=20, alpha=0.5, label='stroke')
-    ax3.axvline(input_data['bmi'][0], color='red', linestyle='dashed', linewidth=2, label='patient bmi')
+    ax3.axvline(int(float(input_data['bmi'][0])), color='red', linestyle='dashed', linewidth=2, label='patient bmi')
     ax3.set_xlabel('bmi')
     ax3.set_ylabel('number of patients')
     ax3.legend()
@@ -129,23 +141,23 @@ elif streamlit.session_state['page'] == 'Result':
     fig5, ax4 = matplotlib.pyplot.subplots()
     ax4.hist(data_frame_cleand[data_frame_cleand['stroke'] == 0]['age'], bins=20, alpha=0.5, label='no stroke')
     ax4.hist(data_frame_cleand[data_frame_cleand['stroke'] == 1]['age'], bins=20, alpha=0.5, label='stroke')
-    ax4.axvline(input_data['age'][0], color='red', linestyle='dashed', linewidth=2, label='patient age')
+    ax4.axvline(int(float(input_data['age'][0])), color='red', linestyle='dashed', linewidth=2, label='patient age')
     ax4.set_xlabel('age')
     ax4.set_ylabel('number of patients')
     ax4.legend()
     streamlit.pyplot(fig5)
 
-    # confusion matrix
-    y_pred = model.predict(X_test)
-    matrix = sklearn.metrics.confusion_matrix(y_test, y_pred)
-    cm_percentage = matrix.astype('float') / matrix.sum(axis=1)[:, numpy.newaxis] * 100
-    # cm_percentage = matrix.astype('float') / matrix.sum() * 100
-    display = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=cm_percentage, display_labels=["no stroke", "stroke"])
-    streamlit.subheader('random tree confusion matrix')
-    # seaborn.heatmap(cm_percentage, annot=True, fmt=".2f", cmap="Blues", xticklabels=["no stroke", "stroke"], yticklabels=["no stroke", "stroke"])
-    figure, ax = matplotlib.pyplot.subplots(figsize=(5, 5))
-    display.plot(ax=ax, values_format=".2f")
-    streamlit.pyplot(figure)
+    # # confusion matrix
+    # y_pred = model.predict(X_test)
+    # matrix = sklearn.metrics.confusion_matrix(y_test, y_pred)
+    # cm_percentage = matrix.astype('float') / matrix.sum(axis=1)[:, numpy.newaxis] * 100
+    # # cm_percentage = matrix.astype('float') / matrix.sum() * 100
+    # display = sklearn.metrics.ConfusionMatrixDisplay(confusion_matrix=cm_percentage, display_labels=["no stroke", "stroke"])
+    # streamlit.subheader('random tree confusion matrix')
+    # # seaborn.heatmap(cm_percentage, annot=True, fmt=".2f", cmap="Blues", xticklabels=["no stroke", "stroke"], yticklabels=["no stroke", "stroke"])
+    # figure, ax = matplotlib.pyplot.subplots(figsize=(5, 5))
+    # display.plot(ax=ax, values_format=".2f")
+    # streamlit.pyplot(figure)
 
     if streamlit.button('Return'):
         streamlit.success('Redirecting to Home page.')
